001bb5b82535e8aa2d06074c57825bf09852673f
mvip2288 <108198907+mvip2288@users.noreply.github.com>
2025-12-08 20:35:55 +0800

    Update URL.java

--- app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
+++ app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
@@ -1,13 +1,13 @@
 package com.github.tvbox.osc.bbox.constant;
 
 public class URL {
-    public static String DOMAIN_NAME_PROXY = "https://raw.bunnyxyz.eu.org/";
+    public static String DOMAIN_NAME_PROXY = "";
     
-    public static final String GITHUB_VERSION_PATH = "https://gitea.com/404ynnub/resource/raw/branch/main/version.json";
+    public static final String GITHUB_VERSION_PATH = "http://1.2.3.4/黑盒更新.json";
     // 默认线路
-    public static final String DEFAULT_API_URL = "http://www.饭太硬.net/tv/";
+    public static final String DEFAULT_API_URL = "http://1.2.3.4";
     // 默认仓库线路
-    public static final String DEFAULT_STORE_API_URL = "https://gitea.com/404ynnub/resource/raw/branch/main/boxsource.json";
+    public static final String DEFAULT_STORE_API_URL = "http://1.2.3.4/仓";
     // 默认直播线路
     public static final String DEFAULT_LIVE_URL = "";
 
